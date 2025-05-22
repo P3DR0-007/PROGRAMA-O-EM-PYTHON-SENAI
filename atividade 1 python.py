792a23cb5df82e46fa75bdb5eb0dfdfe111353d8
P3DR0-007 <pedroxhenrique2009@gmail.com>
2025-05-22 17:42:54 -0300

    Add files via upload

---
+++ atividade 1 python.py
@@ -0,0 +1,7 @@
+# soma de 2
+n1 =float(input('numero 1 :'))
+n2 =float(input('numero 2 :'))
+print('resultado +',n1 + n2)
+print('resultado -',n1 - n2)
+print('resultado *',n1 * n2)
+print('resultado /',n1 / n2)
