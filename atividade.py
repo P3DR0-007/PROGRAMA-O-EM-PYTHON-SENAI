d525e7ac17bd2a61f5750173843bfd3f71d6f796
P3DR0-007 <pedroxhenrique2009@gmail.com>
2025-05-23 17:55:06 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,7 @@
+numero = int(input('digite um numero: '))
+resultado =(numero)**2
+print('resultado: ', resultado)
+
+nome = 'Pedro'
+sobrenome = 'Henrique'
+print (nome, sobrenome)           
---
+++ atividade2.py
@@ -0,0 +1,31 @@
+print('Seja bem vindo!')
+
+verdadeiro = True
+print(type(verdadeiro))
+
+print(10.5 * 2.00)
+
+print (10/2)
+
+print(10-5)
+
+print(10//2)
+
+print(10.0 * 5.0 * 2.6 * 8.0)
+
+numero = 10
+print(numero ** 2)
+
+nome = int(input('Digite seu nome: '))
+idade = int(input('Digite sua idade: '))
+email = input('Digite seu e-mail :')
+endereco = input('Digite seu endereço')
+curso = input('Digite seu curso: ')
+            
+print(f'''
+nome: {nome}
+idade: {idade}
+e-mail: {email}
+enderço: {endereco}
+curso: {curso}
+''')
