d1e9ced9f719ae233115cefde0a4064ccd6183c2
P3DR0-007 <pedroxhenrique2009@gmail.com>
2025-06-05 16:55:23 -0300

    Add files via upload

---
+++ par_ou_impar.py
@@ -0,0 +1,11 @@
+def  comparar():
+     N1 = int(input('Numero 1'))
+     N2 = int(input('Numero 2'))
+     
+     if N1 % 2 == 0 and N2 %2 ==0:
+          print('ambos são pares')
+     else:
+          print('Apenas um é par')
+     
+     
+comparar()
