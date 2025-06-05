1532ccd260a45d1701e8714b3cfdf5d9ba1aa101
P3DR0-007 <pedroxhenrique2009@gmail.com>
2025-06-05 15:54:19 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,40 @@
+def soma(n1,n2):
+          print('=', n1 + n2)
+          
+def sub(n1,n2):
+          print('=', n1 - n2)
+          
+def mult(n1,n2):
+          print('=', n1 * n2)
+          
+def div(n1,n2):
+          print('=', n1 / n2)
+          
+def calculadora( ):
+        n1 = float(input(' =  '))
+        op = input('Escolha a operação +  -  * /')
+        if op == '+':
+               n2 = float(input(' =  '))
+               soma(n1,n2)
+               
+        elif op =='-':
+               n2 = float(input(' =  '))
+               soma(n1,n2)
+               
+        elif op =='*':
+               n2 = float(input(' =  '))
+               soma(n1,n2)
+               
+        elif op =='/':
+               n2 = float(input(' =  '))
+               soma(n1,n2)
+               
+        else:
+                      print('Digite algo válido')
+                      
+while True:
+                    calculadora( )
+                    
+                    input('Digite enter para sair!')
+               
+          
